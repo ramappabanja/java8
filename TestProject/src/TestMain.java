29ce1c092380c2bb849992daa73b2bcffe1c6338
Ram <ramappa.banja@gmail.com>
2018-11-09 23:51:12 +0530

    My First GIT Project

---
+++ TestProject/src/TestMain.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author nisum
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("ENjoy the GIT Features");
+	}
+
+}
